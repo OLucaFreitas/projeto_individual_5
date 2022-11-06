f88885111cc545ac2f2c34b165bf7b9f99aaea4f
OLucaFreitas <lucasseverino13@hotmail.com>
2022-11-06 17:44:49 -0300

    Primeiro commit

---
+++ Backend/Model/Promocao.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Infrastructure;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+
+namespace GoodTravel.Model
+{
+    [Table("promocoes")]
+    public class Promocao
+    {
+        private readonly ILazyLoader _lazyLoader;
+
+        public Promocao()
+        {
+
+        }
+
+        public Promocao(ILazyLoader lazyLoader)
+        {
+            _lazyLoader = lazyLoader;
+        }
+
+
+        [Column("id")]
+        [Required]
+        public int Id { get; set; }
+
+        
+        [Required]
+        public string Nome { get; set; }
+
+        [Required]
+        public double valorPromo { get; set; }
+
+
+        private Destino _Destino;
+        
+        [ForeignKey("destino_fk")]
+        public Destino Destino
+        {
+            get => _lazyLoader.Load(this, ref _Destino);
+            set => _Destino = value;
+        }
+
+    }
+}
